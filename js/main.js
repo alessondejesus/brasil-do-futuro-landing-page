6d71d0e1e84b46baab1d1216d4c216e8061e5312
Álesson de Jesus <alesson.jesus@synapsebrasil.com.br>
2021-02-02 09:33:56 -0500

    template

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var counterDown = $("#counter-down");
+        
+counterDown.counter({
+    autoStart: false,
+    countFrom: localStorage.count ? localStorage.count : 200,
+    countTo: 1,
+    runOnce: true,
+    duration: 10800000,
+    easing: "easeOutCubic"
+});
+
+counterDown.counter("start");
+
+if (!localStorage.count) {
+    localStorage.setItem('count', 200)
+}
+
+setInterval(function () {
+    if(localStorage.count > 1){
+        localStorage.count = Number(localStorage.count) - 1
+    }
+    console.log(Number(localStorage.count))
+}, 20000);
